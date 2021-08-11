735fd2cc09cfae0712d12503fd010b5d46a623f5
moreirathomas <moreirathomas97@gmail.com>
2021-08-11 10:46:17 +0200

    Use polymorphism to dissociate items behavior
    - add specialized item structs
    - define and implement Updater interface
    -  abstract update behavior with multiplier variable (n+1 = n + (1*m)), with code duplication
    - write tests for Update methods

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -1,8 +1,133 @@
 package main
 
+// Item represents an item in the Gilded Rose inn.
 type Item struct {
-	name            string
-	sellIn, quality int
+	name    string
+	sellIn  int // sellIn denotes the number of days available to sell the item.
+	quality int // quality denotes how valuable the item is.
+}
+
+// CommonItem represents an item which has no special behavior.
+type CommonItem struct {
+	Item
+	multiplier int
+}
+
+// RareItem represents an item which has a special behavior during its daily quality update.
+// The multiplier is obtained through the MultiplierFunc, allowing for configurable behaviors.
+type RareItem struct {
+	Item
+	MultiplierFunc MultiplierFunc
+}
+
+// LegendaryItem represents an item which never has to be sold or decreases in quality.
+type LegendaryItem struct {
+	Item
+}
+
+// ConjuredItem represents an item which degrade in quality twice as fast as common items.
+type ConjuredItem struct {
+	Item
+	multiplier int
+}
+
+// MultiplierFunc represents a function to compute the update mutlitplier from an item.
+type MultiplierFunc func(*Item) int
+
+// Updater is an interface for an item which changes in quality at the end of each day.
+type Updater interface {
+	Update()
+}
+
+// newItem returns a new item.
+func newItem(name string, sellIn, quality int) Item {
+	return Item{
+		name:    name,
+		sellIn:  sellIn,
+		quality: quality,
+	}
+}
+
+// NewCommonItem returns a new common item. Its quality multiplier is -1.
+func NewCommonItem(name string, sellIn, quality int) *CommonItem {
+	return &CommonItem{
+		Item:       newItem(name, sellIn, quality),
+		multiplier: -1,
+	}
+}
+
+// Update drops the item sell by date and quality by 1.
+// If the sell by date has passed, the quality drop is doubled.
+func (i *CommonItem) Update() {
+	i.sellIn = i.sellIn - 1
+
+	if i.quality < 0 || i.quality >= 50 {
+		return
+	}
+
+	m := i.multiplier
+	if i.sellIn < 0 {
+		m = m * 2
+	}
+
+	i.quality = i.quality + 1*m
+}
+
+// NewRareItem returns a new rare item. Its quality multiplier is provided by multiplierFunc.
+func NewRareItem(name string, sellIn, quality int, multiplierFunc MultiplierFunc) *RareItem {
+	return &RareItem{
+		Item:           newItem(name, sellIn, quality),
+		MultiplierFunc: multiplierFunc,
+	}
+}
+
+// Update drops the item sell by date and updates its quality.
+// MultiplierFunc is used to compute the quality change.
+func (i *RareItem) Update() {
+	i.sellIn = i.sellIn - 1
+
+	if i.quality < 0 || i.quality >= 50 {
+		return
+	}
+
+	m := i.MultiplierFunc(&i.Item)
+
+	i.quality = i.quality + 1*m
+}
+
+// NewRareItem returns a new legendary item. It has no quality multiplier.
+func NewLegendaryItem(name string, sellIn, quality int) *LegendaryItem {
+	return &LegendaryItem{
+		Item: newItem(name, sellIn, quality),
+	}
+}
+
+// Update does nothing to the legendary item.
+func (i *LegendaryItem) Update() {}
+
+// NewConjuredItem returns a new conjured item. Its quality multiplier is -2.
+func NewConjuredItem(name string, sellIn, quality int) *ConjuredItem {
+	return &ConjuredItem{
+		Item:       newItem(name, sellIn, quality),
+		multiplier: -2,
+	}
+}
+
+// Update drops the item sell by date and quality by 2.
+// If the sell by date has passed, the quality drop is doubled.
+func (i *ConjuredItem) Update() {
+	i.sellIn = i.sellIn - 1
+
+	if i.quality < 0 || i.quality >= 50 {
+		return
+	}
+
+	m := i.multiplier
+	if i.sellIn < 0 {
+		m = m * 2
+	}
+
+	i.quality = i.quality + 1*m
 }
 
 func UpdateQuality(items []*Item) {
--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -2,14 +2,149 @@ package main
 
 import "testing"
 
-func Test_Foo(t *testing.T) {
-	var items = []*Item{
-		&Item{"foo", 0, 0},
+func TestCommonItem(t *testing.T) {
+	item := NewCommonItem("Elixir of the Mongoose", 1, 5)
+
+	item.Update()
+	if item.sellIn != 0 {
+		t.Errorf("SellIn: Expected %d but got %d", 0, item.sellIn)
+	}
+	if item.quality != 4 {
+		t.Errorf("Quality: Expected %d but got %d", 4, item.quality)
 	}
 
-	UpdateQuality(items)
+	// Once the sell by date has passed, Quality degrades twice as fast.
+	item.Update()
+	if item.sellIn != -1 {
+		t.Errorf("SellIn: Expected %d but got %d", -1, item.sellIn)
+	}
+	if item.quality != 2 {
+		t.Errorf("Quality: Expected %d but got %d", 2, item.quality)
+	}
+}
 
-	if items[0].name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
+func TestRareItem(t *testing.T) {
+	multiplierFunc := func(i *Item) int {
+		m := 1
+		if i.sellIn < 0 {
+			m = m * 2
+		}
+		return m
+	}
+
+	item := NewRareItem("Aged Brie", 1, 0, multiplierFunc)
+
+	item.Update()
+	if item.sellIn != 0 {
+		t.Errorf("SellIn: Expected %d but got %d", 0, item.sellIn)
+	}
+	if item.quality != 1 {
+		t.Errorf("Quality: Expected %d but got %d", 1, item.quality)
+	}
+
+	// Once the sell by date has passed, Quality upgrades twice as fast.
+	item.Update()
+	if item.sellIn != -1 {
+		t.Errorf("SellIn: Expected %d but got %d", -1, item.sellIn)
+	}
+	if item.quality != 3 {
+		t.Errorf("Quality: Expected %d but got %d", 3, item.quality)
+	}
+
+	multiplierFunc = func(i *Item) int {
+		switch {
+		case i.sellIn < 0:
+			return -i.quality
+		case i.sellIn <= 5:
+			return 5
+		case i.sellIn <= 10:
+			return 2
+		default:
+			return 1
+		}
+	}
+	item = NewRareItem("Backstage passes to a TAFKAL80ETC concert", 15, 20, multiplierFunc)
+
+	// "Backstage passes" increases in Quality as its SellIn value approaches.
+	item.Update()
+	if item.sellIn != 14 {
+		t.Errorf("SellIn: Expected %d but got %d", 14, item.sellIn)
+	}
+	if item.quality != 21 {
+		t.Errorf("Quality: Expected %d but got %d", 21, item.quality)
+	}
+
+	// Quality increases by 2 when there are 10 days or less.
+	item = NewRareItem("Backstage passes to a TAFKAL80ETC concert", 10, 20, multiplierFunc)
+	item.Update()
+	if item.sellIn != 9 {
+		t.Errorf("SellIn: Expected %d but got %d", 9, item.sellIn)
+	}
+	if item.quality != 22 {
+		t.Errorf("Quality: Expected %d but got %d", 22, item.quality)
+	}
+
+	// Quality increases by 3 when there are 5 days or less
+	item = NewRareItem("Backstage passes to a TAFKAL80ETC concert", 5, 20, multiplierFunc)
+	item.Update()
+	if item.sellIn != 4 {
+		t.Errorf("SellIn: Expected %d but got %d", 4, item.sellIn)
+	}
+	if item.quality != 25 {
+		t.Errorf("Quality: Expected %d but got %d", 25, item.quality)
+	}
+
+	// Quality drops to 0 after the concert
+	item = NewRareItem("Backstage passes to a TAFKAL80ETC concert", 0, 20, multiplierFunc)
+	item.Update()
+	if item.sellIn != -1 {
+		t.Errorf("SellIn: Expected %d but got %d", -1, item.sellIn)
+	}
+	if item.quality != 0 {
+		t.Errorf("Quality: Expected %d but got %d", 0, item.quality)
+	}
+}
+
+func TestLegendaryItem(t *testing.T) {
+	item := NewLegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80)
+
+	item.Update()
+	if item.sellIn != 0 {
+		t.Errorf("SellIn: Expected %d but got %d", 0, item.sellIn)
+	}
+	if item.quality != 80 {
+		t.Errorf("Quality: Expected %d but got %d", 80, item.quality)
+	}
+
+	// Negative sellIn are allowed.
+	item = NewLegendaryItem("Sulfuras, Hand of Ragnaros", -1, 80)
+
+	item.Update()
+	if item.sellIn != -1 {
+		t.Errorf("SellIn: Expected %d but got %d", -1, item.sellIn)
+	}
+	if item.quality != 80 {
+		t.Errorf("Quality: Expected %d but got %d", 80, item.quality)
+	}
+}
+
+func TestConjuredItem(t *testing.T) {
+	item := NewConjuredItem("Conjured Mana Cake", 1, 6)
+
+	item.Update()
+	if item.sellIn != 0 {
+		t.Errorf("SellIn: Expected %d but got %d", 0, item.sellIn)
+	}
+	if item.quality != 4 {
+		t.Errorf("Quality: Expected %d but got %d", 4, item.quality)
+	}
+
+	// Once the sell by date has passed, Quality degrades twice as fast.
+	item.Update()
+	if item.sellIn != -1 {
+		t.Errorf("SellIn: Expected %d but got %d", -1, item.sellIn)
+	}
+	if item.quality != 0 {
+		t.Errorf("Quality: Expected %d but got %d", 0, item.quality)
 	}
 }
